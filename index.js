e93bd9d08d91b55d6e4b527d45116af80bcc22ca
Link1515 <terrylin1515@gmail.com>
2026-02-04 13:53:55 +0800

    feat: generate per-date room svgs and metadata files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import fs from 'fs/promises'
 import path from 'path'
 import { stringify } from 'svgson'
+import { dateToString } from './utils/dateToString.js'
 import generateSchedule from './lib/schedule-json-generator/generateSchedule.js'
 import { scheduleTemplate } from './template/scheduleTemplate.js'
 
@@ -15,12 +16,45 @@ const scheduleStr = await generateSchedule({
 })
 
 const schedule = JSON.parse(scheduleStr)
+const dates = new Set()
+const rooms = new Set()
+
+schedule.sessions.forEach(session => {
+  const startDate = new Date(session.start)
+  dates.add(dateToString(startDate))
+  rooms.add(session.room)
+})
+
+const sessionGroup = {}
+schedule.sessions.forEach(session => {
+  const date = dateToString(new Date(session.start))
+  const room = session.room
+  const groupName = `${date}-${room}`
+
+  if (!Object.hasOwn(sessionGroup, groupName)) {
+    sessionGroup[groupName] = []
+  }
+
+  sessionGroup[groupName].push(session)
+})
+
+const svgs = []
+for (const groupName in sessionGroup) {
+  sessionGroup[groupName].sort((a, b) => new Date(a.start) - new Date(b.start))
+  const svgJson = scheduleTemplate(schedule, sessionGroup[groupName])
+  svgs.push({
+    name: `${groupName}.svg`,
+    content: stringify(svgJson)
+  })
+}
 
 await fs.mkdir(outputDir, { recursive: true })
-await fs.writeFile(path.resolve(outputDir, 'schedule.json'), scheduleStr)
-
-const sessions = schedule.sessions.slice(0, 3)
-const svgJson = scheduleTemplate(schedule, sessions)
-const svg = stringify(svgJson)
-
-await fs.writeFile(path.resolve(outputDir, 'schedule.svg'), svg)
+const tasks = []
+tasks.push(fs.writeFile(path.resolve(outputDir, 'schedule.json'), scheduleStr))
+tasks.push(fs.writeFile(path.resolve(outputDir, 'dates.json'), JSON.stringify(Array.from(dates))))
+tasks.push(fs.writeFile(path.resolve(outputDir, 'rooms.json'), JSON.stringify(Array.from(rooms))))
+svgs.forEach(svg => {
+  tasks.push(fs.writeFile(path.resolve(outputDir, svg.name), svg.content))
+})
+await Promise.all(tasks)
+console.log('Done!')
---
+++ utils/dateToString.js
@@ -0,0 +1,9 @@
+/**
+ * @param {Date} date
+ */
+export function dateToString(date) {
+  const y = date.getFullYear()
+  const m = String(date.getMonth() + 1).padStart(2, '0')
+  const d = String(date.getDate()).padStart(2, '0')
+  return `${y}-${m}-${d}`
+}
