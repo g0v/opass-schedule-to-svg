5f9f136e72d7bfd9438cc77cc31f56b6d24aafd0
Link1515 <terrylin1515@gmail.com>
2026-02-13 15:26:52 +0800

    chore: set vite base path for project name

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ import vue from '@vitejs/plugin-vue'
 import path from 'node:path'
 import sirv from 'sirv'
 
+const PROJECT_NAME = 'opass-schedule-to-svg'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${PROJECT_NAME}/`,
   plugins: [
     vue(),
     tailwindcss(),
@@ -15,7 +18,7 @@ export default defineConfig({
       apply: 'serve',
       configureServer(server) {
         const distDataDir = path.resolve(process.cwd(), 'dist', 'data')
-        server.middlewares.use('/data', sirv(distDataDir, { dev: true, etag: true }))
+        server.middlewares.use(`/${PROJECT_NAME}/data`, sirv(distDataDir, { dev: true, etag: true }))
       },
     },
   ],
